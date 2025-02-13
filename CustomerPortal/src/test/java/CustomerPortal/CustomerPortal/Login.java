87d7187cb31f3dee6a07b13a69c3ab838a248e09
Priyanka <qa.priyanka.gupta@gmail.com>
2025-02-13 11:31:31 +0530

    POM for Login

--- CustomerPortal/src/test/java/CustomerPortal/CustomerPortal/Login.java
+++ CustomerPortal/src/test/java/CustomerPortal/CustomerPortal/Login.java
@@ -1,5 +1,6 @@
 package CustomerPortal.CustomerPortal;
 
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -10,27 +11,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import Pages.LoginPage;
 
 public class Login {
+	private static WebDriver driver=null;
 	public static void main(String[] args) {
-		//WebDriverManager.chromedriver().browserVersion("133").setup();
 		WebDriverManager.chromedriver().setup();
 
-		WebDriver driver=new ChromeDriver();
+		driver=new ChromeDriver();
 		
 		try {
 		    driver.get("https://customerportal.birdzi.com/en/web/guest/home");
 		    driver.manage().window().maximize();
 		    Thread.sleep(2000);
-		   		    
-		    WebElement username=driver.findElement(By.id("_58_login"));
-		    username.sendKeys("pgupta+coborns@birdzi.com");
+		   		      
+		    LoginPage.email(driver).sendKeys("pgupta+coborns@birdzi.com");
+		    LoginPage.password(driver).sendKeys("Shiv123!@#");
 		    
-		    WebElement password=driver.findElement(By.id("_58_password"));
-		    password.sendKeys("Shiv123!@#");
-		    password.sendKeys(Keys.RETURN);   
+		    LoginPage.password(driver).sendKeys(Keys.RETURN);   
 		    
 		    WebElement offersElement = driver.findElement(By.xpath("//span[text()='Offers']"));
 
@@ -52,5 +51,4 @@ public class Login {
 		driver.close();
 		driver.quit();
 	}
-
 }
